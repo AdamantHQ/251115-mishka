a54f0341f44ff48ca01e466876e7015f13d96492
Alexandr <alexandradamant@gmail.com>
2016-12-19 11:41:08 +0300

    Переделал карты и добавил маркер

---
+++ js/map.js
@@ -0,0 +1,16 @@
+function initMap() {
+  var mapCenter = {lat: 59.9367, lng: 30.3215};
+  var mishka = {lat: 59.9363, lng: 30.3217};
+
+  var map = new google.maps.Map(document.querySelector(".contacts__map-container"), {
+    zoom: 16,
+    center: mapCenter
+  });
+
+  var marker = new google.maps.Marker({
+    position: mishka,
+    map: map,
+    title: "Мишка: Милые штуки ручной работы для дома",
+    icon: "../img/icon-map-pin.svg"
+  });
+}
